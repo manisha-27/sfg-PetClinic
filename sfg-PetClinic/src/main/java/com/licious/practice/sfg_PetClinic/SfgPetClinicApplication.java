c4d55ed4f985cb9d978569df0ff9eb02a45b2d8b
manishasingh <manisha270417@gmail.com>
2024-11-14 17:01:01 +0530

    Initial Project Add

---
+++ sfg-PetClinic/src/main/java/com/licious/practice/sfg_PetClinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.licious.practice.sfg_PetClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicApplication.class, args);
+	}
+
+}
